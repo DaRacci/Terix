626f99377c88ef17d4dfdb32650bb1862524e15a
Racci <racci@sylphmc.com>
2021-09-29 21:30:28 +1000

    New PotionFactory

--- src/main/kotlin/me/racci/sylphia/factories/PotionFactory.kt
+++ src/main/kotlin/me/racci/sylphia/factories/PotionFactory.kt
@@ -1,5 +1,8 @@
 package me.racci.sylphia.factories
 
+import me.racci.sylphia.enums.Condition
+import me.racci.sylphia.utils.PotionUtils
+import me.racci.sylphia.utils.PrivatePotionEffectType
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
@@ -18,11 +21,18 @@ class PotionFactory {
             1,
             true,
             false,
-            false
+            false,
+            Condition.LEVITATION
         )
+    }
 
+    companion object {
 
-
+        fun newPotion(string: String, condition: Condition) : PotionEffect? {
+            val potion = string.split(":".toRegex()).toTypedArray()
+            if ((potion[1].toIntOrNull() ?: return null) !in 0..if(PotionUtils.isValid(potion[0])) PrivatePotionEffectType.valueOf(potion[0]).maxLevel else return null) return null
+            return PotionEffect(PotionEffectType.getByName(potion[0])!!, Int.MAX_VALUE, potion[1].toInt(), true, false, false, condition)
+        }
 
     }
 
