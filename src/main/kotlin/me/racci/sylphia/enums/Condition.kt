0b2a5b60c11c32a967d7aabe5fc5aecde51233a6
Racci <racci@sylphmc.com>
2021-09-29 21:29:19 +1000

    New Conditions Conditions for the Attribute Handler and Potion Handler

---
+++ src/main/kotlin/me/racci/sylphia/enums/Condition.kt
@@ -0,0 +1,23 @@
+package me.racci.sylphia.enums
+
+enum class Condition {
+
+    /**
+     * Origin Passive Conditions
+     */
+    PARENT,
+    DAY,
+    NIGHT,
+    WATER,
+    LAVA,
+    OVERWORLD,
+    NETHER,
+    END,
+    /**
+     * Origin Ability Conditions
+     */
+    LEVITATION,
+
+
+
+}
