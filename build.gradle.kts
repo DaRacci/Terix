0b0907a4f749b33624e4908c8022a197681737e6
Da_Racci <tangentmoons@gmail.com>
2021-10-24 00:38:55 +1100

    chore: Push

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,8 @@ version = "0.0.4"
 dependencies {
 
     compileOnly(project(":RacciCore"))
+    compileOnly("me.clip:placeholderapi:2.10.10")
+    compileOnly("com.github.angeschossen:LandsAPI:5.15.2")
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
 
 }
