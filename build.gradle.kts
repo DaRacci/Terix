2f72d5cf9cf5f2ce636d8bf4c1eef7571eb64490
Racci <racci@racci.dev>
2022-02-13 17:37:00 +1100

    build(gradle): Use Minix Conventions and lots of other changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,97 +1,113 @@
+
+val minixConventions: String by project
+val minixVersion: String by project
+val kotlinVersion: String by project
+
 plugins {
-    java
-    idea
-    `java-library`
-    `maven-publish`
-    kotlin("jvm")                       version "1.6.0-RC2"
-    id("org.jetbrains.dokka")               version "1.5.31"
-    kotlin("plugin.serialization")      version "1.5.31"
-    id("com.github.johnrengelman.shadow")   version "7.1.0"
+    id("dev.racci.minix.kotlin")
+    id("dev.racci.minix.copyjar")
+    id("dev.racci.minix.testing")
+    id("dev.racci.minix.purpurmc")
+    kotlin("plugin.serialization")
+    id("dev.racci.minix.publication")
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+    id("dev.racci.minix.nms")
 }
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-    maven("https://jitpack.io")
-    maven("https://repo.pl3x.net/")
-    maven("https://dl.bintray.com/kotlin/kotlin-dev/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+bukkit {
+    name = "Origins"
+    prefix = "Origins"
+    author = "Racci"
+    apiVersion = "1.18"
+    version = rootProject.version.toString()
+    main = "dev.racci.sylphia.core.Sylphia"
+    load = net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.STARTUP
+    depend = listOf(
+        "Minix",
+        "ProtocolLib"
+    )
+    softDepend = listOf(
+        "PlaceholderAPI",
+        "Lands",
+    )
+    website = "https://minix.racci.dev/"
 }
 
 dependencies {
-
-    compileOnly("me.racci:RacciCore:0.2.4")
-
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0-RC2")
-    compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.6.0-RC2")
-
-    compileOnly("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.2-native-mt")
-
-    compileOnly("net.luckperms:api:5.3")
-    compileOnly("me.clip:placeholderapi:2.10.10")
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
-
+    implementation(project(":Sylphia-Core"))
 }
 
-tasks {
+subprojects {
 
-    processResources {
-        from(sourceSets.main.get().resources.srcDirs) {
-            filesMatching("plugin.yml") {
-                expand("version" to project.version)}
-            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    apply(plugin = "dev.racci.minix.kotlin")
+    apply(plugin = "dev.racci.minix.purpurmc")
+    apply(plugin = "kotlinx-serialization")
+
+    dependencies {
+        compileOnly("dev.racci:Minix:1.0.5")
+        compileOnly("org.purpurmc.purpur:purpur-api:1.18.1-R0.1-SNAPSHOT")
+
+        // Kotlin
+        compileOnly(rootProject.libs.kotlin.stdlib)
+        compileOnly(rootProject.libs.kotlin.reflect)
+
+        // KotlinX
+        compileOnly(rootProject.libs.kotlinx.dateTime)
+        compileOnly(rootProject.libs.kotlinx.coroutines)
+        compileOnly(rootProject.libs.kotlinx.serialization.kaml)
+        compileOnly(rootProject.libs.kotlinx.immutableCollections)
+
+        // Minecraft Libs
+        compileOnly(rootProject.libs.adventure.kotlin)
+        compileOnly(rootProject.libs.adventure.minimessage)
+
+        // Extras
+        compileOnly(rootProject.libs.koin.core)
+    }
+
+    tasks {
+        compileKotlin {
+            kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
         }
     }
-
-    compileKotlin {
-        kotlinOptions.suppressWarnings = true
-        kotlinOptions.jvmTarget = "17"
-        kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
-    }
-
-    withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
-        options.release.set(17)
-    }
-
-    val devServer by registering(Jar::class) {
-        dependsOn(shadowJar)
-        destinationDirectory.set(File("${System.getProperty("user.home")}/Desktop/Minecraft/Sylph/Development/plugins/"))
-        archiveClassifier.set("all")
-        from(zipTree(shadowJar.get().outputs.files.singleFile))
-    }
-
-    val sourcesJar by registering(Jar::class) {
-        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-        archiveClassifier.set("sources")
-        from(sourceSets["main"].allSource)
-    }
-
-    val javadocJar by registering(Jar::class) {
-        dependsOn("dokkaJavadoc")
-        archiveClassifier.set("javadoc")
-        from(dokkaJavadoc.get().outputDirectory)
-    }
-
-    dokkaGfm {
-        outputDirectory.set(File("$buildDir/../docs"))
-    }
-
-    artifacts {
-        archives(sourcesJar)
-        archives(javadocJar)
-    }
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+allprojects {
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.racci.dev/releases")
+        maven("https://repo.codemc.org/repository/maven-public/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+
+    dependencies {
+        compileOnly(rootProject.libs.kotlin.stdlib)
+    }
+
+    ktlint {
+        enableExperimentalRules.set(false)
+    }
+
+    tasks.shadowJar {
+        val location = "dev.racci.sylphia.libs"
+        dependencyFilter.exclude {
+            it.moduleGroup == "org.jetbrains.kotlin" ||
+                it.moduleGroup == "org.jetbrains.intellij" ||
+                it.moduleGroup == "org.jetbrains" ||
+                it.moduleName == "adventure-api" ||
+                it.moduleName == "adventure-text-serializer-*" ||
+                it.moduleName == "adventure-key" ||
+                it.moduleName == "examination-*"
+        }
+        relocate("dev.jorel.commandapi", "$location.commandapi")
+        relocate("net.kyori.adventure.text.minimessage", "dev.racci.minix.libs.kyori.minimessage")
+        relocate("net.kyori.adventure.text.extra.kotlin", "dev.racci.minix.libs.kyroi.kotlin")
+        relocate("org.valiktor", "$location.valiktor")
+        relocate("dev.racci.minix.nms", "$location.minix-nms")
     }
 }
-
-group = findProperty("group")!!
-version = findProperty("version")!!
