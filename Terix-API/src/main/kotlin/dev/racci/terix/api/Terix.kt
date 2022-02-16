c961035da693c7d20d3f87357618193021085057
Racci <racci@racci.dev>
2022-02-16 16:58:48 +1100

    chore: Publish API

---
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/Terix.kt
@@ -0,0 +1,7 @@
+package dev.racci.terix.api
+
+import dev.racci.minix.api.plugin.MinixPlugin
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
+abstract class Terix : MinixPlugin()
---
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/events/PlayerOriginChangeEvent.kt
@@ -0,0 +1,11 @@
+package dev.racci.terix.api.events
+
+import dev.racci.minix.api.events.KPlayerEvent
+import dev.racci.terix.api.origins.AbstractOrigin
+import org.bukkit.entity.Player
+
+class PlayerOriginChangeEvent(
+    player: Player,
+    val preOrigin: AbstractOrigin?,
+    val newOrigin: AbstractOrigin
+) : KPlayerEvent(player, true)
