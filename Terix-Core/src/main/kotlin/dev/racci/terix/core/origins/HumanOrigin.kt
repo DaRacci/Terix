5b3e2916ffaa8865e2324012cde42d484dc9d722
Racci <racci@racci.dev>
2022-02-26 22:13:37 +1100

    chore: Update to new interface and add placeholder item for GUI

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/HumanOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/HumanOrigin.kt
@@ -1,8 +1,7 @@
 package dev.racci.terix.core.origins
 
-import dev.racci.minix.api.builders.ItemBuilderDSL
 import dev.racci.minix.api.extensions.parse
-import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.terix.api.Terix
 import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.enums.Trigger
 import net.kyori.adventure.key.Key
@@ -10,17 +9,12 @@ import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
 import org.bukkit.potion.PotionEffectType
 
-class HumanOrigin(plugin: MinixPlugin) : AbstractOrigin(plugin) {
+class HumanOrigin(override val plugin: Terix) : AbstractOrigin() {
 
-    override val name = "Human"
-    override val colour: NamedTextColor = NamedTextColor.GRAY
-    override val hurtSound = Key.key("minecraft", "entity.player.hurt")
-    override val deathSound = Key.key("minecraft", "entity.player.death")
-    override val guiItem by lazy {
-        ItemBuilderDSL.from(Material.APPLE) {
-            name = displayName
-        }
-    }
+    override val name by lazy { "Human" }
+    override val colour by lazy { NamedTextColor.GRAY!! }
+    override val hurtSound by lazy { Key.key("minecraft", "entity.player.hurt") }
+    override val deathSound by lazy { Key.key("minecraft", "entity.player.death") }
 
     override suspend fun onRegister() {
         potions {
@@ -35,6 +29,13 @@ class HumanOrigin(plugin: MinixPlugin) : AbstractOrigin(plugin) {
             Trigger.DAY causes {
                 title = "<blue>Day".parse()
                 subtitle = "<aqua>haha its bright blue now".parse()
+            }
+        }
+        item {
+            named(displayName)
+            material(Material.APPLE)
+            lore {
+                this[0] = "<gray>This is a human origin".parse()
             }
         }
     }
