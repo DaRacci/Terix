2e0bb54347ef39bed8849c267142925d79f63d66
DaRacci <tangentmoons@gmail.com>
2022-04-05 00:43:34 +1000

    chore: Move lang to data file

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/data/Lang.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/data/Lang.kt
@@ -34,21 +34,21 @@ class Lang {
 
     class Origins {
 
-        var broadcast: Component = "".parse()
+        var broadcast: Component = "<prefix_server><player> has become the <new_origin> origin!".parse()
 
-        var setSelf: Component = "".parse()
+        var setSelf: Component = "<prefix_origins>You set your origin to <new_origin>.".parse()
 
-        var setOther: Component = "".parse()
+        var setOther: Component = "<prefix_origins>Set <target>'s origin to <new_origin>.".parse()
 
-        var setSameSelf: Component = "".parse()
+        var setSameSelf: Component = "<prefix_origins>You are already the <origin> origin!".parse()
 
-        var setSameOther: Component = "".parse()
+        var setSameOther: Component = "<prefix_origins><player> is already the <origin> origin!".parse()
 
-        var getSelf: Component = "".parse()
+        var getSelf: Component = "<prefix_origins>Your origin is <origin>.".parse()
 
-        var getOther: Component = "".parse()
+        var getOther: Component = "<prefix_origins><target>'s origin is <origin>.".parse()
 
-        var nightVision: Component = "".parse()
+        var nightVision: Component = "<prefix_origins>Your night vision now triggers on: <new_nightvision>.".parse()
     }
 
 }
