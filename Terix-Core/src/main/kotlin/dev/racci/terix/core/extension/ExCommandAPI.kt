5ffabad48898d10eb94156c3edb6b70f04225b02
DaRacci <racci@racci.dev>
2022-04-15 21:56:06 +1000

    feat: Add arg builder

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/extension/ExCommandAPI.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/extension/ExCommandAPI.kt
@@ -1,9 +1,11 @@
 package dev.racci.terix.core.extension
 
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.arguments.Argument
 import dev.jorel.commandapi.executors.CommandExecutor
 import dev.jorel.commandapi.executors.ConsoleCommandExecutor
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
+import dev.racci.minix.api.annotations.MinixDsl
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
@@ -20,6 +22,27 @@ fun CommandAPICommand.subcommand(name: String, block: CommandAPICommand.() -> Un
     command.block()
     subcommands.add(command)
     return this
+}
+
+class ArgBuilder {
+    val args by lazy { mutableListOf<Argument>() }
+
+    inline fun <reified A : Argument> arg(name: String, vararg data: Any?): A {
+        val arg = try {
+            A::class.constructors.first { it.parameters.size == (data.size + 1) }.call(name, *data)
+        } catch (e: Exception) {
+            throw RuntimeException("Could not construct argument of type ${A::class.qualifiedName} with the params ${data.joinToString(", ")}.")
+        }
+        args += arg
+        return arg
+    }
+}
+
+@MinixDsl
+fun CommandAPICommand.arguments(block: ArgBuilder.() -> Unit) {
+    val builder = ArgBuilder()
+    builder.block()
+    arguments.addAll(builder.args)
 }
 
 fun CommandAPICommand.execute(block: (CommandSender, Array<out Any>) -> Unit): CommandAPICommand {
