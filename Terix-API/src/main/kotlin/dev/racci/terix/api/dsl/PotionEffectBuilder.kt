0f5a01d7b99f0bf1e75d89aa24880e7ee4bd0706
DaRacci <racci@racci.dev>
2022-05-10 21:12:02 +1000

    refactor: namespaced key generation

--- Terix-API/src/main/kotlin/dev/racci/terix/api/dsl/PotionEffectBuilder.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/dsl/PotionEffectBuilder.kt
@@ -2,11 +2,15 @@ package dev.racci.terix.api.dsl
 
 import dev.racci.minix.api.extensions.inWholeTicks
 import dev.racci.minix.api.extensions.ticks
+import dev.racci.terix.api.OriginService
+import dev.racci.terix.api.origins.AbstractAbility
 import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.enums.Trigger
+import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
+import kotlin.reflect.KClass
 import kotlin.time.Duration
 
 class PotionEffectBuilder() {
@@ -24,26 +28,44 @@ class PotionEffectBuilder() {
     var icon: Boolean? = null
     var key: NamespacedKey? = null
 
-    fun originKey(origin: AbstractOrigin, trigger: Trigger) {
-        this.key = NamespacedKey("terix", "origin_potion_${origin.name.lowercase()}_${trigger.name.lowercase()}")
-    }
+    inline fun <reified O : AbstractOrigin> originKey(trigger: Trigger) = originKey(OriginService.getService().getOrigin(O::class), trigger)
 
-    fun originKey(origin: String, trigger: String) {
+    fun originKey(origin: AbstractOrigin, trigger: Trigger) = originKey(origin.name, trigger.name)
+
+    fun originKey(origin: String, trigger: String): PotionEffectBuilder {
         this.key = NamespacedKey("terix", "origin_potion_${origin.lowercase()}_${trigger.lowercase()}")
+        return this
     }
 
-    fun build(): PotionEffect =
-        PotionEffect(
-            type ?: error("Type must be set for potion builder."),
-            durationInt ?: duration.inWholeTicks.toInt(),
-            amplifier,
-            ambient,
-            particles ?: !ambient,
-            icon ?: particles ?: !ambient,
-            key.takeUnless { it == null || !it.toString().matches(regex) } ?: error("Invalid key. Was null or didn't match terix:origin_potion_(?<origin>[a-z0-9/_.-]+)_(?<trigger>[a-z0-9/_.-]+): $key")
-        )
+    inline fun <reified A : AbstractAbility> abilityKey() = abilityKey(A::class)
+
+    fun abilityKey(ability: KClass<out AbstractAbility>) = abilityKey(ability.simpleName ?: error("Cannot use anonymous classes as abilities."))
+
+    fun abilityKey(ability: String): PotionEffectBuilder {
+        this.key = NamespacedKey("terix", "origin_ability_${ability.lowercase()}")
+        return this
+    }
+
+    fun foodKey(food: Material) = foodKey(food.name)
+
+    fun foodKey(food: String): PotionEffectBuilder {
+        this.key = NamespacedKey("terix", "origin_food_${food.lowercase()}")
+        return this
+    }
+
+    fun build(): PotionEffect = PotionEffect(
+        type ?: error("Type must be set for potion builder."),
+        durationInt ?: duration.inWholeTicks.coerceAtMost(Integer.MAX_VALUE.toLong()).toInt(),
+        amplifier,
+        ambient,
+        particles ?: !ambient,
+        icon ?: particles ?: !ambient,
+        key.takeUnless { it == null || !it.toString().matches(regex) } ?: error("Invalid key. Was null or didn't match terix:${regex.pattern}: $key")
+    )
 
     companion object {
-        val regex by lazy { Regex("terix:origin_potion_(?<origin>[a-z0-9/_.-]+)_(?<trigger>[a-z0-9/_.-]+)") }
+        val regex by lazy { Regex("terix:origin_(?<type>potion|ability)_(?<origin>[a-z\\d/_.-]+_)?(?<trigger>[a-z\\d/_.-]+)") }
+
+        fun build(builder: PotionEffectBuilder.() -> Unit): PotionEffect = PotionEffectBuilder(builder).build()
     }
 }
