b9fe7fb40436ade30ac76027fd2fc66aae545152
DaRacci <racci@racci.dev>
2022-05-10 21:15:46 +1000

    chore: Add sun resistance file

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/enchantments/SunResistance.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/enchantments/SunResistance.kt
@@ -3,7 +3,7 @@ package dev.racci.terix.core.enchantments
 import com.willfp.ecoenchants.enchantments.EcoEnchant
 import com.willfp.ecoenchants.enchantments.meta.EnchantmentType
 
-class SunResistance : EcoEnchant("sun_resistance", EnchantmentType.NORMAL) {
+class SunResistance : EcoEnchant("sun-resistance", EnchantmentType.NORMAL) {
     init {
         instance = this
     }
