aea38601720f98f6fbd38497e7d934702703360c
DaRacci <racci@racci.dev>
2022-05-11 22:02:35 +1000

    feat: Add ensureAsync function

--- Terix-API/src/main/kotlin/dev/racci/terix/api/Utils.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/Utils.kt
@@ -1,18 +1,29 @@
 package dev.racci.terix.api
 
+import dev.racci.minix.api.coroutine.asyncDispatcher
 import dev.racci.minix.api.coroutine.minecraftDispatcher
 import dev.racci.minix.api.extensions.server
 import dev.racci.minix.api.utils.getKoin
 import kotlinx.coroutines.withContext
 
-@PublishedApi
-internal val mainThread by lazy { getKoin().get<Terix>().minecraftDispatcher }
+@PublishedApi internal val mainThread by lazy { getKoin().get<Terix>().minecraftDispatcher }
+@PublishedApi internal val asyncThread by lazy { getKoin().get<Terix>().asyncDispatcher }
 
-suspend inline fun ensureMainThread(crossinline block: () -> Unit) {
+suspend inline fun ensureMainThread(crossinline block: suspend () -> Unit) {
     if (server.isPrimaryThread) {
         block()
     } else {
         withContext(mainThread) {
+            block()
+        }
+    }
+}
+
+suspend inline fun ensureAsync(crossinline block: suspend () -> Unit) {
+    if (!server.isPrimaryThread) {
+        block()
+    } else {
+        withContext(asyncThread) {
             block()
         }
     }
