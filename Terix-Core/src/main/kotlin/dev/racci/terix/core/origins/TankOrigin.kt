d144fbace63654eb1d2d69b7282f9c4b23cc2d0c
DaRacci <racci@racci.dev>
2022-05-29 02:58:57 +1000

    feat: placeholder tank origin

---
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/TankOrigin.kt
@@ -0,0 +1,6 @@
+package dev.racci.terix.core.origins
+
+import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.terix.api.origins.AbstractOrigin
+
+class TankOrigin(override val plugin: MinixPlugin) : AbstractOrigin()
