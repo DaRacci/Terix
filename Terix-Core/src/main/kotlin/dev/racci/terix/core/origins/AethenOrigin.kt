5fbaae59ccc268eab3cf459d78085161c87da0f3
DaRacci <racci@racci.dev>
2022-05-29 02:49:29 +1000

    refactor: rename angel to aethen, modify sounds

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/AethenOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/AethenOrigin.kt
@@ -5,22 +5,24 @@ import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.abilities.Levitate
 import dev.racci.terix.api.origins.enums.KeyBinding
 import dev.racci.terix.api.origins.enums.Trigger
-import net.kyori.adventure.key.Key
+import dev.racci.terix.api.origins.sounds.SoundEffect
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Material
 import org.bukkit.attribute.Attribute
 import org.bukkit.event.entity.EntityDamageEvent
 
-class AngelOrigin(override val plugin: Terix) : AbstractOrigin() {
+class AethenOrigin(override val plugin: Terix) : AbstractOrigin() {
 
-    override val name = "Angel"
+    override val name = "Aethen"
     override val colour = TextColor.fromHexString("#fff6cc")!!
-    override val hurtSound = Key.key("entity.bat.hurt")
-    override val deathSound = Key.key("entity.bat.death")
 
     override suspend fun onRegister() {
+        sounds.hurtSound = SoundEffect("entity.silverfish.hurt")
+        sounds.deathSound = SoundEffect("entity.turtle.death_baby")
+        sounds.ambientSound = SoundEffect("entity.vex.ambient")
+
         attributes {
-            Attribute.GENERIC_MAX_HEALTH *= 0.85
+            Attribute.GENERIC_MAX_HEALTH *= 0.80
             Pair(Trigger.NETHER, Attribute.GENERIC_MAX_HEALTH) *= 0.50
         }
         damage {
