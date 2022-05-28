807524e37803ef6c87c455557983a832c9554b55
DaRacci <racci@racci.dev>
2022-05-29 03:01:53 +1000

    refactor: clean up this mess of a class

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/TriggerInvoker.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/TriggerInvoker.kt
@@ -1,186 +1,79 @@
 package dev.racci.terix.core.origins
 
-import dev.racci.minix.api.extensions.msg
-import dev.racci.minix.api.utils.kotlin.ifNotEmpty
-import dev.racci.minix.api.utils.kotlin.ifTrue
+import dev.racci.minix.api.utils.getKoin
+import dev.racci.terix.api.Terix
 import dev.racci.terix.api.dsl.PotionEffectBuilder
+import dev.racci.terix.api.ensureMainThread
 import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.enums.Trigger
-import dev.racci.terix.core.data.PlayerData
-import dev.racci.terix.core.extension.getLightOrDarkTrigger
-import dev.racci.terix.core.extension.origin
-import dev.racci.terix.core.extension.safelyAddPotions
-import dev.racci.terix.core.extension.safelyRemovePotions
-import dev.racci.terix.core.extension.safelySwapPotions
+import dev.racci.terix.core.extensions.activeTriggers
+import dev.racci.terix.core.extensions.fromOrigin
+import dev.racci.terix.core.extensions.nightVision
+import dev.racci.terix.core.extensions.origin
 import org.bukkit.attribute.Attribute
 import org.bukkit.attribute.AttributeModifier
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
-import org.jetbrains.exposed.sql.transactions.transaction
 import kotlin.time.Duration
 
 suspend fun Trigger.invokeAdd(
     player: Player,
     origin: AbstractOrigin = player.origin()
 ) {
-    val potions = origin.potions[this] ?: mutableListOf()
-
-    if (origin.nightVision) {
-        potions += PotionEffectBuilder {
-            type = PotionEffectType.NIGHT_VISION
-            duration = Duration.INFINITE
-            amplifier = 0
-            ambient = true
-            originKey(origin, this@invokeAdd)
-        }.build()
-    }
-
-    origin.titles[this]?.invoke(player)
-    origin.triggerBlocks[this]?.invoke(player)
-
-    potions.ifNotEmpty(player::safelyAddPotions)
-    origin.attributeModifiers[this]?.let(player::addAttributeModifiers)
+    getKoin().get<Terix>().log.debug { "Activating Trigger: $name" }
+    applyAsyncable(player, origin, this, false)
+    applyMainThread(player, origin, this, false)
 }
 
-suspend fun Trigger.Companion.invokeAddAll(
+/* suspend fun Trigger.Companion.invokeAddAll(
     player: Player,
     origin: AbstractOrigin = player.origin(),
     triggers: Collection<Trigger>
 ) {
     val potions = mutableListOf<PotionEffect>()
-    val attributeModifiers = mutableListOf<Pair<Attribute, AttributeModifier>>()
 
     for (trigger in triggers) {
-        origin.titles[trigger]?.invoke(player)
-        origin.triggerBlocks[trigger]?.invoke(player)
-
+        applyAsyncable(player, origin, trigger, false)
         origin.potions[trigger]?.let(potions::addAll)
-        origin.attributeModifiers[trigger]?.let(attributeModifiers::addAll)
     }
 
-    origin.nightVision.ifTrue {
-        val nightVision = transaction { PlayerData[player].nightVision }
-        if (nightVision !in triggers) return@ifTrue
-        PotionEffectBuilder {
-            type = PotionEffectType.NIGHT_VISION
-            duration = Duration.INFINITE
-            amplifier = 0
-            ambient = true
-            originKey(origin, nightVision)
-        }.build().apply { potions += this }
-    }
-
-    player.safelyAddPotions(potions)
-    player.addAttributeModifiers(attributeModifiers)
-}
+    ensureMainThread { triggers.forEach { applyMainThread(player, origin, it, false) } }
+} */
 
 suspend fun Trigger.invokeSwap(
     oldTrigger: Trigger,
     player: Player,
     origin: AbstractOrigin = player.origin()
 ) {
-    val removePotions = player.getAllOriginPotions(oldTrigger)
-    val removeModifiers = origin.attributeModifiers[oldTrigger]
-    var addPotions = origin.potions[this]?.toMutableList()
-    val addModifiers = origin.attributeModifiers[this]
-
-    origin.titles[this]?.invoke(player)
-    origin.triggerBlocks[this]?.invoke(player)
-
-    if (origin.nightVision && transaction { PlayerData[player].nightVision == this@invokeSwap }) {
-        PotionEffectBuilder {
-            type = PotionEffectType.NIGHT_VISION
-            duration = Duration.INFINITE
-            amplifier = 0
-            ambient = true
-            originKey(origin, this@invokeSwap)
-        }.build().apply { addPotions?.add(this) ?: run { addPotions = mutableListOf(this) } }
+    getKoin().get<Terix>().log.debug { "Swapping Trigger from $oldTrigger to $this" }
+    applyAsyncable(player, origin, oldTrigger, true)
+    applyAsyncable(player, origin, this, false)
+    ensureMainThread {
+        applyMainThread(player, origin, oldTrigger, true)
+        applyMainThread(player, origin, this, false)
     }
-
-    player.safelySwapPotions(removePotions, addPotions)
-    removeModifiers?.let(player::removeAttributeModifiers)
-    addModifiers?.let(player::addAttributeModifiers)
 }
 
-fun Trigger.Companion.invokeRemovalFor(
-    player: Player,
-    vararg triggers: Trigger
-) {
-    val potions = mutableListOf<PotionEffectType>()
-    val attributeModifiers = mutableListOf<Pair<Attribute, AttributeModifier>>()
-    val origin = player.origin()
-
-    for (trigger in triggers) {
-        potions.addAll(player.getAllOriginPotions(trigger))
-        origin.attributeModifiers[trigger]?.let(attributeModifiers::addAll)
-    }
-
-    player.safelyRemovePotions(potions)
-    player.removeAttributeModifiers(attributeModifiers)
-}
-
-fun Trigger.invokeRemove(
+suspend fun Trigger.invokeRemove(
     player: Player,
     origin: AbstractOrigin = player.origin()
 ) {
-    val potions = player.getAllOriginPotions(this)
-
-    player.safelyRemovePotions(potions)
-
-    origin.attributeModifiers[this]?.let(player::removeAttributeModifiers)
+    getKoin().get<Terix>().log.debug { "Deactivating Trigger: $name" }
+    applyAsyncable(player, origin, this, true)
+    applyMainThread(player, origin, this, true)
 }
 
-fun Trigger.Companion.invokeCompleteClean(
+suspend fun Trigger.Companion.invokeBase(
     player: Player,
     origin: AbstractOrigin = player.origin()
 ) {
-    // These shouldn't be needed but are here just in case.
-    player.setCanBreathUnderwater(false)
-    player.setImmuneToFire(false)
-
-    val potions = player.getAllOriginPotions()
-    println("Potions being removed: ${potions.map { it.name }}")
-
-    player.safelyRemovePotions(potions)
-
-    Attribute.values().forEach { attribute ->
-        player.getAttribute(attribute)?.let { inst ->
-            inst.modifiers.filter {
-                it.name.startsWith("origin_modifier_").ifTrue {
-                    player.msg("Removing modifier: ${it.name}")
-                }
-            }.forEach(inst::removeModifier)
-        }
-    }
-
-    for (collection in origin.attributeModifiers.values.filterNotNull()) {
-        println("Removing ${collection.size} modifiers")
-        player.removeAttributeModifiers(collection)
-    }
-}
-
-fun Trigger.Companion.invokeBase(
-    player: Player,
-    origin: AbstractOrigin = player.origin()
-) {
-    var basePotions = origin.potions[Trigger.ON]
+    getKoin().get<Terix>().log.debug { "Adding base" }
+    applyAsyncable(player, origin, Trigger.ON, false)
+    applyMainThread(player, origin, Trigger.ON, false)
 
     player.setCanBreathUnderwater(origin.waterBreathing)
     player.setImmuneToFire(origin.fireImmune)
-
-    if (origin.nightVision && transaction { PlayerData[player].nightVision == Trigger.ON }) {
-        PotionEffectBuilder {
-            type = PotionEffectType.NIGHT_VISION
-            duration = Duration.INFINITE
-            amplifier = 0
-            ambient = true
-            originKey(origin.name.lowercase(), "on")
-        }.build().apply { basePotions?.add(this) ?: run { basePotions = mutableListOf(this) } }
-    }
-
-    basePotions?.let { player.safelyAddPotions(it) }
-    origin.attributeModifiers[Trigger.ON]?.let(player::addAttributeModifiers)
 }
 
 suspend fun Trigger.Companion.invokeReload(
@@ -188,37 +81,101 @@ suspend fun Trigger.Companion.invokeReload(
     oldOrigin: AbstractOrigin? = null,
     newOrigin: AbstractOrigin = player.origin()
 ) {
-    invokeCompleteClean(player, oldOrigin ?: newOrigin)
-    invokeBase(player, newOrigin)
+    getKoin().get<Terix>().log.debug { "Changing origins from ${oldOrigin?.name ?: "null"} to ${newOrigin.name}" }
+    val activeTriggers = player.activeTriggers()
+    val removePotions = arrayListOf<PotionEffectType>()
+    val curHealth = player.health
 
-    val triggers = mutableListOf<Trigger>()
-    triggers += player.world.environment.getTrigger()
-    player.world.getTimeTrigger()?.let(triggers::add)
-    player.getLiquidTrigger().let(triggers::addAll)
-    player.getLightOrDarkTrigger()?.let(triggers::add)
+    if (oldOrigin != null) {
+        activeTriggers.forEach { applyAsyncable(player, oldOrigin, it, true) }
+        activeTriggers.map(player::getAllOriginPotions).forEach(removePotions::addAll)
+    }
 
-    Trigger.invokeAddAll(player, newOrigin, triggers)
+    ensureMainThread {
+        removePotions.forEach(player::removePotionEffect)
+        activeTriggers.forEach { newOrigin.potions[it]?.let(player::addPotionEffects) }
+        when {
+            oldOrigin?.nightVision == true && !newOrigin.nightVision -> {
+                removePotions += PotionEffectType.NIGHT_VISION
+            }
+            oldOrigin?.nightVision != true && newOrigin.nightVision -> {
+                val trigger = activeTriggers.find { it == player.nightVision } ?: return@ensureMainThread
+                player.addPotionEffect(nightVisionPotion(newOrigin, trigger))
+            }
+        }
+    }
+
+    activeTriggers.forEach { applyAsyncable(player, newOrigin, it, false) }
+    if (player.health < curHealth) player.health = curHealth.coerceAtMost(player.maxHealth)
 }
 
-private fun Player.getAllOriginPotions(trigger: Trigger? = null): Collection<PotionEffectType> =
-    activePotionEffects.filter { effect ->
-        effect.hasKey() && PotionEffectBuilder.regex.find(effect.key!!.asString())?.let { match ->
-            return@let if (trigger != null) {
-                match.groups[2]?.value == trigger.name.lowercase()
-            } else true
-        } == true
-    }.map(PotionEffect::getType)
+fun Player.getAllOriginPotions(trigger: Trigger? = null): Collection<PotionEffectType> =
+    activePotionEffects
+        .filter { effect ->
+            effect.hasKey() && PotionEffectBuilder.regex.find(effect.key!!.asString())?.let { match ->
+                match.groups["type"]!!.value == "potion" &&
+                    trigger == null || match.groups["trigger"]!!.value == trigger!!.name.lowercase()
+            } ?: false
+        }.map(PotionEffect::getType)
 
-private fun Player.removeAttributeModifiers(collection: Collection<Pair<Attribute, AttributeModifier>>) {
+internal suspend fun applyAsyncable(
+    player: Player,
+    origin: AbstractOrigin,
+    trigger: Trigger,
+    remove: Boolean
+) {
+    if (remove) {
+        origin.attributeModifiers[trigger]?.let(player::removeAttributeModifiers)
+        return
+    }
+    origin.titles[trigger]?.invoke(player)
+    origin.triggerBlocks[trigger]?.invoke(player)
+    origin.attributeModifiers[trigger]?.let(player::addAttributeModifiers)
+}
+
+// TODO: Cache if the player has their nightvision potion
+internal suspend fun applyMainThread(
+    player: Player,
+    origin: AbstractOrigin,
+    trigger: Trigger,
+    remove: Boolean
+) = ensureMainThread {
+    if (remove) {
+        origin.potions[trigger]?.takeUnless(Collection<*>::isEmpty)?.map(PotionEffect::getType)?.forEach(player::removePotionEffect)
+        if (origin.nightVision &&
+            player.nightVision == trigger &&
+            player.activePotionEffects.find { it.type == PotionEffectType.NIGHT_VISION }?.fromOrigin() == true
+        ) player.removePotionEffect(PotionEffectType.NIGHT_VISION)
+        return@ensureMainThread
+    }
+    origin.potions[trigger]?.takeUnless(Collection<*>::isEmpty)?.let(player::addPotionEffects)
+    if (origin.nightVision &&
+        player.nightVision == trigger &&
+        player.activePotionEffects.find { it.type == PotionEffectType.NIGHT_VISION }?.fromOrigin() != true
+    ) { player.addPotionEffect(nightVisionPotion(origin, trigger)) }
+}
+
+private fun nightVisionPotion(
+    origin: AbstractOrigin,
+    trigger: Trigger
+) = PotionEffectBuilder {
+    type = PotionEffectType.NIGHT_VISION
+    duration = Duration.INFINITE
+    amplifier = 0
+    ambient = true
+    originKey(origin, trigger)
+}.build()
+
+internal fun Player.removeAttributeModifiers(collection: Collection<Pair<Attribute, AttributeModifier>>) {
     for ((attribute, modifier) in collection) {
-        println("Removing modifier ${modifier.serialize()}")
+        getKoin().get<Terix>().log.debug { "Removing attribute modifier ${modifier.name} to ${attribute.name}" }
         getAttribute(attribute)?.removeModifier(modifier)
     }
 }
 
-private fun Player.addAttributeModifiers(collection: Collection<Pair<Attribute, AttributeModifier>>) {
+internal fun Player.addAttributeModifiers(collection: Collection<Pair<Attribute, AttributeModifier>>) {
     for ((attribute, modifier) in collection) {
-        println("Adding modifier ${modifier.serialize()}")
+        getKoin().get<Terix>().log.debug { "Adding attribute modifier ${modifier.name} to ${attribute.name}" }
         getAttribute(attribute)?.addModifier(modifier)
     }
 }
