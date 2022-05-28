2def8877c97d09df9944c56be9f36bdddee6f0e5
DaRacci <racci@racci.dev>
2022-05-29 03:04:27 +1000

    refactor: ahuoljs

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
@@ -6,6 +6,7 @@ import dev.racci.terix.api.origins.enums.Trigger
 import dev.racci.terix.api.origins.sounds.SoundEffect
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 
 class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
 
@@ -20,10 +21,19 @@ class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
 
         damage {
             Trigger.WET += 2.0
+            listOf(
+                DamageCause.LAVA,
+                DamageCause.FIRE,
+                DamageCause.MELTING,
+                DamageCause.FIRE_TICK,
+                DamageCause.HOT_FLOOR
+            ) *= 0.0
         }
         item {
             material = Material.LARGE_AMETHYST_BUD
             lore = "<light_purple>A breath of fire that can be used to summon a dragon."
         }
     }
+
+    // TODO: Explosion when shield is broken
 }
