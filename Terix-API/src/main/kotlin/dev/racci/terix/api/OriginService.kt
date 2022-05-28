300f783d725db8747790e9567e2875a7c384d1fc
DaRacci <racci@racci.dev>
2022-05-29 02:58:03 +1000

    refactor: make origin service interface

--- Terix-API/src/main/kotlin/dev/racci/terix/api/OriginService.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/OriginService.kt
@@ -1,28 +1,28 @@
 package dev.racci.terix.api
 
-import dev.racci.minix.api.extension.Extension
+import dev.racci.minix.api.utils.getKoin
 import dev.racci.terix.api.origins.AbstractAbility
 import dev.racci.terix.api.origins.AbstractOrigin
 import kotlinx.collections.immutable.PersistentMap
 import kotlin.reflect.KClass
 
-abstract class OriginService : Extension<Terix>() {
+interface OriginService {
 
-    abstract fun getAbilities(): PersistentMap<KClass<out AbstractAbility>, AbstractAbility>
+    fun getAbilities(): PersistentMap<KClass<out AbstractAbility>, AbstractAbility>
 
-    abstract fun getOrigins(): PersistentMap<KClass<out AbstractOrigin>, AbstractOrigin>
+    fun getOrigins(): PersistentMap<KClass<out AbstractOrigin>, AbstractOrigin>
 
-    abstract fun getOrigin(origin: KClass<out AbstractOrigin>): AbstractOrigin
+    fun getOrigin(origin: KClass<out AbstractOrigin>): AbstractOrigin
 
-    abstract fun getOriginOrNull(origin: KClass<out AbstractOrigin>): AbstractOrigin?
+    fun getOriginOrNull(origin: KClass<out AbstractOrigin>): AbstractOrigin?
 
-    abstract fun getAbility(ability: KClass<out AbstractAbility>): AbstractAbility
+    fun getAbility(ability: KClass<out AbstractAbility>): AbstractAbility
 
-    abstract fun getAbilityOrNull(ability: KClass<out AbstractAbility>): AbstractAbility?
+    fun getAbilityOrNull(ability: KClass<out AbstractAbility>): AbstractAbility?
 
-    abstract fun getOrigin(name: String): AbstractOrigin
+    fun getOrigin(name: String): AbstractOrigin
 
-    abstract fun getOriginOrNull(name: String?): AbstractOrigin?
+    fun getOriginOrNull(name: String?): AbstractOrigin?
 
-    companion object : ExtensionCompanion<OriginService>()
+    companion object : OriginService by getKoin().get()
 }
