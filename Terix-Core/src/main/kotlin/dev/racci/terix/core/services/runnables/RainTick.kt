6b185ce6325b09f81877ca594d99cc18f0b8ac18
DaRacci <racci@racci.dev>
2022-06-01 18:49:41 +1000

    refactor: damage every second time for rain and water.

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/services/runnables/RainTick.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/services/runnables/RainTick.kt
@@ -1,11 +1,14 @@
 package dev.racci.terix.core.services.runnables
 
 import dev.racci.minix.api.extensions.sync
+import dev.racci.minix.api.utils.now
+import dev.racci.minix.api.utils.ticks
 import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.enums.Trigger
 import dev.racci.terix.core.extensions.inRain
 import dev.racci.terix.core.extensions.wasInRain
 import dev.racci.terix.core.services.RunnableService
+import kotlinx.datetime.Instant
 import org.bukkit.entity.Player
 
 class RainTick(
@@ -15,11 +18,15 @@ class RainTick(
     mother: MotherCoroutineRunnable
 ) : ChildCoroutineRunnable(mother) {
 
+    private var lastTick = Instant.DISTANT_PAST
+
     override suspend fun run() {
         player.wasInRain = player.inRain
         player.inRain = player.isInRain
         service.doInvoke(player, origin, Trigger.RAIN, player.wasInRain, player.inRain)
+
         if (!player.inRain) return
+        if ((now() - lastTick).ticks < 10) return
 
         val wet = origin.damageTicks[Trigger.WET]
         val rain = origin.damageTicks[Trigger.RAIN]
@@ -27,7 +34,7 @@ class RainTick(
 
         // Get the one which isn't null or whichever is higher
         val ticks = if (wet == null) rain!! else if (rain == null) wet else maxOf(wet, rain)
-
+        lastTick = now()
         service.sync { player.damage(ticks) }
     }
 }
--- Terix-Core/src/main/kotlin/dev/racci/terix/core/services/runnables/WaterTick.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/services/runnables/WaterTick.kt
@@ -3,11 +3,14 @@ package dev.racci.terix.core.services.runnables
 import dev.racci.minix.api.events.LiquidType
 import dev.racci.minix.api.events.LiquidType.Companion.liquidType
 import dev.racci.minix.api.extensions.sync
+import dev.racci.minix.api.utils.now
+import dev.racci.minix.api.utils.ticks
 import dev.racci.terix.api.origins.AbstractOrigin
 import dev.racci.terix.api.origins.enums.Trigger
 import dev.racci.terix.core.extensions.inWater
 import dev.racci.terix.core.extensions.wasInWater
 import dev.racci.terix.core.services.RunnableService
+import kotlinx.datetime.Instant
 import org.bukkit.entity.Player
 
 class WaterTick(
@@ -17,11 +20,15 @@ class WaterTick(
     mother: MotherCoroutineRunnable
 ) : ChildCoroutineRunnable(mother) {
 
+    private var lastTick = Instant.DISTANT_PAST
+
     override suspend fun run() {
         player.wasInWater = player.inWater
         player.inWater = player.location.block.liquidType == LiquidType.WATER
         service.doInvoke(player, origin, Trigger.WATER, player.wasInWater, player.inWater)
+
         if (!player.inWater) return
+        if ((now() - lastTick).ticks < 10) return
 
         val wet = origin.damageTicks[Trigger.WET]
         val water = origin.damageTicks[Trigger.WATER]
@@ -30,6 +37,7 @@ class WaterTick(
         // Get the one which isn't null or whichever is higher
         val ticks = if (wet == null) water!! else if (water == null) wet else maxOf(wet, water)
 
+        lastTick = now()
         service.sync { player.damage(ticks) }
     }
 }
