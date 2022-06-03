19d44d5288f930ed9609db1df8f49cb7055d0e46
DaRacci <racci@racci.dev>
2022-06-04 01:11:54 +1000

    chore: add todo notes

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/TankOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/TankOrigin.kt
@@ -3,4 +3,6 @@ package dev.racci.terix.core.origins
 import dev.racci.minix.api.plugin.MinixPlugin
 import dev.racci.terix.api.origins.AbstractOrigin
 
+// TODO -> Add base armour value
+// TODO -> Break stone with hands
 class TankOrigin(override val plugin: MinixPlugin) : AbstractOrigin()
