0bf1112b222beb6c6063f2e70edf48dec9221a8b
DaRacci <racci@racci.dev>
2022-06-04 01:11:23 +1000

    refactor: remove water and fire buffs and debuffs

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
@@ -2,11 +2,9 @@ package dev.racci.terix.core.origins
 
 import dev.racci.terix.api.Terix
 import dev.racci.terix.api.origins.AbstractOrigin
-import dev.racci.terix.api.origins.enums.Trigger
 import dev.racci.terix.api.origins.sounds.SoundEffect
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Material
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 
 class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
 
@@ -18,16 +16,6 @@ class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
         sounds.deathSound = SoundEffect("entity.ravager.stunned")
         sounds.ambientSound = SoundEffect("entity.strider.ambient")
 
-        damage {
-            Trigger.WET += 2.0
-            listOf(
-                DamageCause.LAVA,
-                DamageCause.FIRE,
-                DamageCause.MELTING,
-                DamageCause.FIRE_TICK,
-                DamageCause.HOT_FLOOR
-            ) *= 0.0
-        }
         item {
             material = Material.LARGE_AMETHYST_BUD
             lore = "<light_purple>A breath of fire that can be used to summon a dragon."
