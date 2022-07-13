2ed599a1fecc4905eac8a2dbf913ccb28fb3c7a7
DaRacci <racci@racci.dev>
2022-07-12 22:29:24 -0700

    chore: Cog

---
+++ scripts/pre-bump.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# If previous version and version arent set then exit
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Previous version or current not set"
+  exit 1
+fi
+
+if ! ./gradlew test --info; then
+   echo "Tests failed, aborting"
+   exit 1;
+fi
+
+echo "Bumping from v${1} to v${2}!"
