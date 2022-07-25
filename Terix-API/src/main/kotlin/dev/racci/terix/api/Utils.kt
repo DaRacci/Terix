5ec4b1f31321e9398b4d011fd0966d984224c3c6
DaRacci <racci@racci.dev>
2022-07-24 23:21:31 -0700

    refactor: Move PlayerData.kt into API

--- Terix-API/src/main/kotlin/dev/racci/terix/api/Utils.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/Utils.kt
@@ -4,7 +4,10 @@ import dev.racci.minix.api.coroutine.asyncDispatcher
 import dev.racci.minix.api.coroutine.minecraftDispatcher
 import dev.racci.minix.api.extensions.server
 import dev.racci.minix.api.utils.getKoin
+import io.sentry.protocol.User
 import kotlinx.coroutines.withContext
+import org.bukkit.entity.Player
+import java.util.UUID
 
 @PublishedApi internal val mainThread by lazy { getKoin().get<Terix>().minecraftDispatcher }
 @PublishedApi internal val asyncThread by lazy { getKoin().get<Terix>().asyncDispatcher }
@@ -27,4 +30,22 @@ suspend inline fun ensureAsync(crossinline block: suspend () -> Unit) {
             block()
         }
     }
+}
+
+fun origin(player: Player) = PlayerData.cachedOrigin(player)
+
+private val sentryUsers = hashMapOf<UUID, User>()
+
+fun Player.sentryUser() = sentryUsers.getOrPut(uniqueId) {
+    val user = User()
+    user.id = this.uniqueId.toString()
+    user.username = this.name
+    user.ipAddress = this.address.address.hostAddress
+    user.others = mapOf(
+        "minecraft_protocol" to this.protocolVersion.toString(),
+        "minecraft_brand" to this.clientBrandName.orEmpty(),
+        "terix_origin" to origin(this).name
+    )
+
+    user
 }
