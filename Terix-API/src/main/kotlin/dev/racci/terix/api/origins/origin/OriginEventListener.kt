d62602ae03c8118c142ab683d633f4f0959d0d15
DaRacci <racci@racci.dev>
2022-08-06 14:49:42 -0700

    docs(EventForwarder): add descriptions to events

--- Terix-API/src/main/kotlin/dev/racci/terix/api/origins/origin/OriginEventListener.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/origins/origin/OriginEventListener.kt
@@ -58,111 +58,165 @@ import org.spigotmc.event.player.PlayerSpawnLocationEvent
 @API(status = API.Status.STABLE, since = "1.0.0")
 sealed interface OriginEventListener : KListener<MinixPlugin> {
 
+    /** Called when Terix first registers this origin. */
     suspend fun onRegister() = Unit
 
+    /** Called when the player respawns. */
     suspend fun onRespawn(event: PlayerRespawnEvent) = Unit
 
+    /** Called when the player first becomes this origin. */
     suspend fun onBecomeOrigin(event: PlayerOriginChangeEvent) = Unit
 
-    suspend fun onChange(event: PlayerOriginChangeEvent) = Unit
+    /** Called when the player changes to another origin. */
+    suspend fun onChangeOrigin(event: PlayerOriginChangeEvent) = Unit
 
+    /** Called after the player respawns. */
     suspend fun onPostRespawn(event: PlayerPostRespawnEvent) = Unit
 
+    /** Called when the player dies. */
     suspend fun onDeath(event: PlayerDeathEvent) = Unit
 
+    /** Called when the player enters a liquid. */
     suspend fun onEnterLiquid(event: PlayerEnterLiquidEvent) = Unit
 
+    /** Called when the player exits a liquid. */
     suspend fun onExitLiquid(event: PlayerExitLiquidEvent) = Unit
 
+    /** Called when the player breaks a block. */
     suspend fun onBreakBlock(event: BlockBreakEvent) = Unit
 
+    /** Called when the player places a block. */
     suspend fun onPlaceBlock(event: BlockPlaceEvent) = Unit
 
+    /** Called when the player is damaged by a non-entity source. */
     suspend fun onDamage(event: EntityDamageEvent) = Unit
 
+    /** Called when the player damages another entity. */
     suspend fun onDamageEntity(event: EntityDamageByEntityEvent) = Unit
 
+    /** Called when the player is damaged by another entity. */
     suspend fun onDamageByEntity(event: EntityDamageByEntityEvent) = Unit
 
+    /** Called when the player kills another entity. */
     suspend fun onKillEntity(event: EntityDeathEvent) = Unit
 
+    /** Called when the player launches a projectile. */
     suspend fun onProjectileLaunch(event: PlayerLaunchProjectileEvent) = Unit
 
+    /** Called when the player's projectile lands. */
     suspend fun onProjectileHit(event: ProjectileHitEvent) = Unit
 
+    /** Called when the player's projectile hits an entity. */
     suspend fun onProjectileCollide(event: ProjectileCollideEvent) = Unit
 
+    /** Called when the player changes their armor. */
     suspend fun onArmourChange(event: PlayerArmorChangeEvent) = Unit
 
+    /** Called when the player changes worlds. */
     suspend fun onChangedWorld(event: PlayerChangedWorldEvent) = Unit
 
+    /** Called when the player fishes. */
     suspend fun onFish(event: PlayerFishEvent) = Unit
 
+    /** Called when the player damages an item. */
     suspend fun onItemDamage(event: PlayerItemDamageEvent) = Unit
 
+    /** Called when the player uses riptide on a trident. */
     suspend fun onRiptide(event: PlayerRiptideEvent) = Unit
 
+    /** Called when the player starts to burn. */
     suspend fun onCombust(event: EntityCombustEvent) = Unit
 
+    /** Called when the player is resurrected by a totem. */
     suspend fun onResurrect(event: EntityResurrectEvent) = Unit
 
+    /** Called when the player enters the swimming animation. */
     suspend fun onToggleSwim(event: EntityToggleSwimEvent) = Unit
 
+    /** Called when the player enters the gliding animation. */
     suspend fun onToggleGlide(event: EntityToggleGlideEvent) = Unit
 
+    /** Called when the player jumps. */
     suspend fun onJump(event: PlayerJumpEvent) = Unit
 
+    /** Called when the player is knocked back by another entity. */
     suspend fun onKnockbackByEntity(event: EntityKnockbackByEntityEvent) = Unit
 
+    /** Called when the player knocks back another entity. */
     suspend fun onKnockbackEntity(event: EntityKnockbackByEntityEvent) = Unit
 
+    /** Called when a phantom will spawn due to a player's insomnia. */
     suspend fun onPhantomSpawn(event: PhantomPreSpawnEvent) = Unit
 
+    /** Called when the player boosts an elytra. */
     suspend fun onElytraBoost(event: PlayerElytraBoostEvent) = Unit
 
+    /** Called when the player mounts another entity. */
     suspend fun onMount(event: EntityMountEvent) = Unit
 
+    /** Called when the player dismounts another entity. */
     suspend fun onDismount(event: EntityDismountEvent) = Unit
 
+    /** Called when the player left-clicks. */
     suspend fun onLeftClick(event: PlayerLeftClickEvent) = Unit
 
+    /** Called when the player right-clicks. */
     suspend fun onRightClick(event: PlayerRightClickEvent) = Unit
 
+    /** Called when the player uses their offhand key. */
     suspend fun onOffhand(event: PlayerOffhandEvent) = Unit
 
+    /** Called when the player double-clicks the left-mouse button. */
     suspend fun onDoubleLeftClick(event: PlayerDoubleLeftClickEvent) = Unit
 
+    /** Called when the player double-clicks the right-mouse button. */
     suspend fun onDoubleRightClick(event: PlayerDoubleRightClickEvent) = Unit
 
+    /** Called when the player double-clicks the offhand key. */
     suspend fun onDoubleOffhand(event: PlayerDoubleOffhandEvent) = Unit
 
+    /** Called when the player left-clicks while sneaking. */
     suspend fun onSneakLeftClick(event: PlayerShiftLeftClickEvent) = Unit
 
+    /** Called when the player right-clicks while sneaking. */
     suspend fun onSneakRightClick(event: PlayerShiftRightClickEvent) = Unit
 
+    /** Called when the player uses the offhand key while sneaking. */
     suspend fun onSneakOffhand(event: PlayerShiftOffhandEvent) = Unit
 
+    /** Called when the player double left-clicks while sneaking. */
     suspend fun onSneakDoubleLeftClick(event: PlayerShiftDoubleLeftClickEvent) = Unit
 
+    /** Called when the player double right-clicks while sneaking. */
     suspend fun onSneakDoubleRightClick(event: PlayerShiftDoubleRightClickEvent) = Unit
 
+    /** Called when the player double uses the offhand key while sneaking. */
     suspend fun onSneakDoubleOffhand(event: PlayerShiftDoubleOffhandEvent) = Unit
 
+    /** Called when the player opens an inventory. */
     suspend fun onInventoryOpen(event: InventoryOpenEvent) = Unit
 
+    /** Called when the player's remaining air changes. */
     suspend fun onAirChange(event: EntityAirChangeEvent) = Unit
 
+    /** Called when the player enters a bed. */
     suspend fun onBedEnter(event: PlayerBedEnterEvent) = Unit
 
+    /** Called when the player leaves a bed. */
     suspend fun onBedLeave(event: PlayerBedLeaveEvent) = Unit
 
+    /** Called when the player interacts with something. */
     suspend fun onInteract(event: PlayerInteractEvent) = Unit
 
+    /** Called when the player consumes an item. */
     suspend fun onItemConsume(event: PlayerItemConsumeEvent) = Unit
 
+    /** Called when the player toggles sneaking. */
     suspend fun onToggleSneak(event: PlayerToggleSneakEvent) = Unit
 
+    /** Called when the player spawn location is being found. */
     suspend fun onSpawnLocation(event: PlayerSpawnLocationEvent) = Unit
 
+    /** Called each game tick. */
     suspend fun onTick(player: Player) = Unit
 }
