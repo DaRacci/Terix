98068808a4da7d76306de2e8bb16872ca9ceac6e
DaRacci <racci@racci.dev>
2022-08-06 16:58:47 -0700

    feat(Origin): Base Blizz & Dragon
    Blizz:
    * Snowballs are now an edible food source.
    * Nerfed
    cooked
    fish
    and buffed raw fish.
    * Increase taken fire /
    lava
    damage.
    
    Dragon:
    * Buffed meat saturation.
    * Added constant
    hunger.
    *
    Emerald, Diamond and Gold Ingots are now consumable.
    * Take
    more damage
    from being hit with beds.
    * Take more damage from bed
    explosions.

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/BlizzOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/BlizzOrigin.kt
@@ -1,15 +1,14 @@
 package dev.racci.terix.core.origins
 
 import dev.racci.terix.api.Terix
+import dev.racci.terix.api.dsl.FoodPropertyBuilder
 import dev.racci.terix.api.origins.origin.AbstractOrigin
 import dev.racci.terix.api.origins.sounds.SoundEffect
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Material
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 
-// TODO -> Edible snowballs are the top food.
-// TODO -> Raw fish are better than cooked fish.
 // TODO -> Cake!
-// TODO -> More fire / lava damage
 class BlizzOrigin(override val plugin: Terix) : AbstractOrigin() {
 
     override val name = "Blizz"
@@ -19,6 +18,28 @@ class BlizzOrigin(override val plugin: Terix) : AbstractOrigin() {
         sounds.hurtSound = SoundEffect("entity.panda.bite")
         sounds.deathSound = SoundEffect("entity.squid.death")
         sounds.ambientSound = SoundEffect("entity.skeleton_horse.ambient")
+
+        damage {
+            listOf(
+                DamageCause.FIRE,
+                DamageCause.LAVA,
+                DamageCause.FIRE_TICK,
+                DamageCause.HOT_FLOOR
+            ) *= 2.5
+
+            DamageCause.FREEZE /= 3
+        }
+
+        food {
+            Material.SNOWBALL += { builder: FoodPropertyBuilder ->
+                builder.canAlwaysEat = true
+                builder.nutrition = 3
+                builder.saturationModifier = 3f
+                builder.fastFood = true
+            }
+            listOf(Material.TROPICAL_FISH, Material.PUFFERFISH, Material.SALMON, Material.COD) *= 3
+            listOf(Material.COOKED_SALMON, Material.COOKED_COD) /= 3
+        }
 
         item {
             material = Material.POWDER_SNOW_BUCKET
--- Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/origins/DragonOrigin.kt
@@ -1,16 +1,25 @@
 package dev.racci.terix.core.origins
 
+import com.destroystokyo.paper.MaterialSetTag
+import dev.racci.minix.api.extensions.cancel
+import dev.racci.minix.api.utils.minecraft.MaterialTagsExtension
 import dev.racci.terix.api.Terix
+import dev.racci.terix.api.origins.enums.Trigger
 import dev.racci.terix.api.origins.origin.AbstractOrigin
 import dev.racci.terix.api.origins.sounds.SoundEffect
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Material
+import org.bukkit.entity.LivingEntity
+import org.bukkit.event.entity.EntityDamageByBlockEvent
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerBedEnterEvent
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.potion.PotionEffectType
+import kotlin.time.Duration
 
-// TODO -> Every meat gives increased saturation.
-// TODO -> Always have hunger.
-// TODO -> Diamond, Gold and Emerald are edible.
-// TODO -> Take more damage from beds.
-// TODO -> Bed explosion does more damage.
+// TODO: Explosion when shield is broken
+
 class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
 
     override val name = "Dragon"
@@ -21,11 +30,61 @@ class DragonOrigin(override val plugin: Terix) : AbstractOrigin() {
         sounds.deathSound = SoundEffect("entity.ravager.stunned")
         sounds.ambientSound = SoundEffect("entity.strider.ambient")
 
+        food {
+            listOf(MaterialTagsExtension.RAW_MEATS, MaterialTagsExtension.COOKED_MEATS) *= 2
+            modifyFood(Material.EMERALD) {
+                it.nutrition = 7
+                it.saturationModifier = 3.5f
+            }
+            modifyFood(Material.DIAMOND) {
+                it.nutrition = 5
+                it.saturationModifier = 3f
+            }
+            modifyFood(Material.GOLD_INGOT) {
+                it.nutrition = 3
+                it.saturationModifier = 1.5f
+            }
+        }
+
+        potions {
+            Trigger.ON += {
+                type = PotionEffectType.HUNGER
+                duration = Duration.INFINITE
+                amplifier = 1
+                ambient = true
+            }
+        }
+
         item {
             material = Material.LARGE_AMETHYST_BUD
             lore = "<light_purple>A breath of fire that can be used to summon a dragon."
         }
     }
 
-    // TODO: Explosion when shield is broken
+    override suspend fun onDamageByBlock(event: EntityDamageByBlockEvent) {
+        bedExplosion(event)
+    }
+
+    override suspend fun onDamageByEntity(event: EntityDamageByEntityEvent) {
+        bedDamage(event)
+    }
+
+    override suspend fun onBedEnter(event: PlayerBedEnterEvent) {
+        event.cancel()
+    }
+
+    private fun bedExplosion(event: EntityDamageByBlockEvent) {
+        if (event.cause != EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) return
+        if (!MaterialSetTag.BEDS.isTagged(event.damager?.type ?: return)) return
+
+        event.damage *= 2
+    }
+
+    private fun bedDamage(event: EntityDamageByEntityEvent) {
+        val attacker = event.entity as? LivingEntity ?: return
+        val item = attacker.equipment?.getItem(EquipmentSlot.HAND) ?: return
+        if (!MaterialSetTag.BEDS.isTagged(item.type)) return
+
+        event.damage *= 2
+    }
 }
