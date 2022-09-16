0a8b7b544f527b47fe8ae03d1b94cff697422b9d
DaRacci <racci@racci.dev>
2022-09-16 15:15:12 +1000

    refactor(StorageService): cleanup

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/services/StorageService.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/services/StorageService.kt
@@ -5,42 +5,45 @@ import com.zaxxer.hikari.HikariDataSource
 import dev.racci.minix.api.annotations.MappedExtension
 import dev.racci.minix.api.extension.Extension
 import dev.racci.minix.api.extensions.event
-import dev.racci.minix.api.utils.kotlin.ifInitialized
 import dev.racci.terix.api.OriginService
-import dev.racci.terix.api.PlayerData
 import dev.racci.terix.api.Terix
+import dev.racci.terix.api.TerixPlayer
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.properties.Delegates
 
 @MappedExtension(Terix::class, "Storage Service", [OriginService::class])
 class StorageService(override val plugin: Terix) : Extension<Terix>() {
 
-    private var config = lazy {
-        val config = HikariConfig()
-        config.jdbcUrl = "jdbc:sqlite:${plugin.dataFolder.path}/database.db"
-        config.connectionTestQuery = "SELECT 1"
-        config.addDataSourceProperty("cachePrepStmts", true)
-        config.addDataSourceProperty("prepStmtCacheSize", "250")
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-        config
+    private var dataSource by Delegates.notNull<HikariDataSource>()
+
+    override suspend fun handleLoad() {
+        val config = HikariConfig().apply {
+            this.jdbcUrl = "jdbc:sqlite:${plugin.dataFolder.path}/database.db"
+            this.connectionTestQuery = "SELECT 1"
+            this.addDataSourceProperty("cachePrepStmts", true)
+            this.addDataSourceProperty("prepStmtCacheSize", "250")
+            this.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
+        }
+
+        dataSource = HikariDataSource(config)
     }
-    private var dataSource = lazy { HikariDataSource(config.value) }
 
     override suspend fun handleEnable() {
-        val database = Database.connect(dataSource.value)
-        getKoin().setProperty("terix:database", database)
+        getKoin().setProperty(Terix.DATABASE, Database.connect(dataSource))
 
-        transaction(database) {
-            SchemaUtils.createMissingTablesAndColumns(PlayerData.User)
+        transaction(Terix.database) {
+            SchemaUtils.createMissingTablesAndColumns(TerixPlayer.User)
         }
         event<AsyncPlayerPreLoginEvent> {
-            transaction(database) { PlayerData.findById(this@event.uniqueId) ?: PlayerData.new(this@event.uniqueId) {} }
+            transaction(Terix.database) { TerixPlayer.findById(this@event.uniqueId) ?: TerixPlayer.new(this@event.uniqueId) {} }
         }
     }
 
     override suspend fun handleUnload() {
-        dataSource.ifInitialized(HikariDataSource::close)
+        getKoin().deleteProperty(Terix.DATABASE)
+        dataSource.close()
     }
 }
