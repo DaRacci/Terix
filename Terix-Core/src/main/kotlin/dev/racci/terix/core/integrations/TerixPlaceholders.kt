0f8a79686776c0bab4f424ae02d9a8b8bbdb8e58
DaRacci <racci@racci.dev>
2022-09-16 15:11:00 +1000

    refactor: Use new integration placeholderapi

---
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/integrations/TerixPlaceholders.kt
@@ -0,0 +1,22 @@
+package dev.racci.terix.core.integrations
+
+import dev.racci.minix.api.annotations.MappedIntegration
+import dev.racci.minix.api.integrations.placeholders.PlaceholderIntegration
+import dev.racci.minix.api.integrations.placeholders.PlaceholderManager
+import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.terix.api.Terix
+import dev.racci.terix.api.TerixPlayer
+
+@MappedIntegration(
+    "PlaceholderAPI",
+    Terix::class,
+    PlaceholderManager::class
+)
+class TerixPlaceholders(override val plugin: MinixPlugin) : PlaceholderIntegration() {
+
+    init {
+        registerOnlinePlaceholder("origin_name") { TerixPlayer.cachedOrigin(this).name }
+        registerOnlinePlaceholder("origin_display") { TerixPlayer.cachedOrigin(this).displayName }
+        registerOnlinePlaceholder("origin_colour") { TerixPlayer.cachedOrigin(this).colour }
+    }
+}
