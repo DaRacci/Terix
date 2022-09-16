4e9cc8dc6ea8e1b1b50c936068013fa9b65326fa
DaRacci <racci@racci.dev>
2022-09-16 15:15:12 +1000

    refactor(StorageService): cleanup

--- Terix-API/src/main/kotlin/dev/racci/terix/api/Terix.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/Terix.kt
@@ -1,7 +1,16 @@
 package dev.racci.terix.api
 
 import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.minix.api.utils.getKoin
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.exposed.sql.Database
 
 @ApiStatus.Internal
-abstract class Terix : MinixPlugin()
+abstract class Terix : MinixPlugin() {
+
+    companion object {
+        const val DATABASE = "terix:database"
+
+        val database get() = getKoin().getProperty<Database>(DATABASE)
+    }
+}
