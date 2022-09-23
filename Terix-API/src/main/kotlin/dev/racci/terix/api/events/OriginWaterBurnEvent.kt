7ccabef5ede7e217d226dc3b527bca8f406df137
DaRacci <racci@racci.dev>
2022-09-24 00:26:30 +1000

    feat(API): Origin Events for tickers

---
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/events/OriginWaterBurnEvent.kt
@@ -0,0 +1,14 @@
+package dev.racci.terix.api.events
+
+import dev.racci.terix.api.origins.origin.Origin
+import org.bukkit.entity.Player
+
+class OriginWaterBurnEvent(
+    player: Player,
+    origin: Origin,
+    var damage: Double
+) : OriginEvent(player, origin) {
+    companion object : CompanionEventHandler() {
+        @JvmStatic override fun getHandlerList() = super.getHandlerList()
+    }
+}
