87a7ca58adeae9891b06c86bb8cc5897464c3717
DaRacci <racci@racci.dev>
2022-09-24 00:29:46 +1000

    feat: Update and fix ecoenchants integration

--- Terix-Core/build.gradle.kts
+++ Terix-Core/build.gradle.kts
@@ -4,18 +4,24 @@ plugins {
 
 val minixVersion: String by rootProject
 
+repositories {
+    maven("https://jitpack.io")
+}
+
 dependencies {
     compileOnly(project(":Terix-API"))
     compileOnly(rootProject.libs.minecraft.api.placeholderAPI)
     compileOnly(rootProject.libs.minecraft.api.protoclLib)
     compileOnly(rootProject.libs.minecraft.api.landsAPI)
-    compileOnly(rootProject.libs.minecraft.api.ecoEnchants)
     compileOnly(rootProject.libs.minecraft.api.eco)
     compileOnly(rootProject.libs.minecraft.api.lightAPI)
     compileOnly(rootProject.libs.minecraft.inventoryFramework)
     compileOnly(rootProject.libs.bundles.cloud)
     compileOnly(rootProject.libs.bundles.cloud.kotlin)
     compileOnly(rootProject.libs.minecraft.particles)
+
+    compileOnly("com.willfp:libreforge:3.104.0")
+    compileOnly("com.willfp:EcoEnchants:9.0.0-b3")
 
     testImplementation(project(":Terix-API"))
 }
---
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/integrations/EcoEnchantsIntegration.kt
@@ -0,0 +1,42 @@
+package dev.racci.terix.core.integrations
+
+import com.willfp.ecoenchants.EcoEnchantsPlugin
+import com.willfp.ecoenchants.enchants.EcoEnchant
+import com.willfp.ecoenchants.enchants.EcoEnchants
+import com.willfp.ecoenchants.target.EnchantLookup.hasEnchantInSlot
+import dev.racci.minix.api.extensions.cancel
+import dev.racci.minix.api.extensions.event
+import dev.racci.minix.api.extensions.pluginManager
+import dev.racci.minix.api.extensions.reflection.castOrThrow
+import dev.racci.minix.api.integrations.Integration
+import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.terix.api.events.OriginSunlightBurnEvent
+
+// @MappedIntegration(
+//    "EcoEnchants",
+//    Terix::class
+// )
+class EcoEnchantsIntegration(override val plugin: MinixPlugin) : Integration {
+    private val sunProtection = SunProtectionEnchant(pluginManager.getPlugin("EcoEnchants").castOrThrow())
+
+    override suspend fun handleLoad() {
+        EcoEnchants.register(sunProtection)
+    }
+
+    override suspend fun handleEnable() {
+        event<OriginSunlightBurnEvent> {
+            if (!this.player.hasEnchantInSlot(sunProtection, HELMET_SLOT)) return@event
+            this.cancel()
+        }
+    }
+
+    private class SunProtectionEnchant(plugin: EcoEnchantsPlugin) : EcoEnchant(
+        "sun_protection",
+        plugin,
+        force = true
+    )
+
+    private companion object {
+        const val HELMET_SLOT = 103
+    }
+}
