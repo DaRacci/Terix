b7d2d40188f81ca656ebbd0b8bfe66306073522e
DaRacci <racci@racci.dev>
2022-10-08 17:34:08 +1100

    feat: Improve Levitate ability

--- Terix-API/src/main/kotlin/dev/racci/terix/api/origins/abilities/Levitate.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/origins/abilities/Levitate.kt
@@ -1,9 +1,16 @@
 package dev.racci.terix.api.origins.abilities
 
-import dev.racci.minix.api.extensions.SimpleKListener
+import dev.racci.minix.api.destructors.component1
+import dev.racci.minix.api.destructors.component2
+import dev.racci.minix.api.destructors.component3
+import dev.racci.minix.api.events.PlayerMoveXYZEvent
 import dev.racci.minix.api.extensions.cancel
 import dev.racci.minix.api.extensions.event
+import dev.racci.minix.api.extensions.onlinePlayers
+import dev.racci.minix.api.extensions.taskAsync
+import dev.racci.minix.api.extensions.ticks
 import dev.racci.terix.api.dsl.PotionEffectBuilder
+import dev.racci.terix.api.dsl.dslMutator
 import dev.racci.terix.api.extensions.playSound
 import dev.racci.terix.api.sentryBreadcrumb
 import org.bukkit.NamespacedKey
@@ -12,41 +19,69 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.EntityToggleGlideEvent
 import org.bukkit.potion.PotionEffectType
 import java.util.UUID
+import kotlin.time.Duration
 
+// TODO -> Don't force elytra if the player isn't moving.
+// TODO -> If the player is still only levitate up.
 class Levitate : Ability(AbilityType.TOGGLE) {
 
-    private val glideMap = HashSet<UUID>()
+    private val glideMap = HashSet<Player>()
 
     init {
-        val listener = SimpleKListener(plugin)
-        listener.event<EntityToggleGlideEvent>(
-            EventPriority.LOWEST,
-            true
-        ) {
+        event<PlayerMoveXYZEvent>(EventPriority.MONITOR, true) {
+            if (!this@Levitate.isActivated(this.player.uniqueId)) return@event
+            if (this.from.z == this.to.z && this.from.x == this.to.x) return@event
+
+            startGliding(player)
+            player.velocity.add(player.location.direction.normalize().multiply(3))
+        }
+
+        event<EntityToggleGlideEvent>(EventPriority.LOWEST, true) {
             val player = entity as? Player ?: return@event
-            if (glideMap.contains(player.uniqueId)) cancel()
+            if (glideMap.contains(player)) cancel()
+        }
+
+        val locTracker = HashMap<UUID, Pair<Double, Double>>()
+        taskAsync(repeatDelay = 1.ticks) {
+            if (onlinePlayers.isEmpty()) return@taskAsync
+            if (glideMap.isEmpty()) return@taskAsync
+
+            for (player in glideMap) {
+                if (locTracker.putIfAbsent(player.uniqueId, player.location.y to player.location.y) == null) continue
+
+                locTracker.computeIfPresent(player.uniqueId) { _, (lastX, lastZ) ->
+                    val (newX, _, newZ) = player.location
+                    val diffX = newX - lastX
+                    val diffZ = newZ - lastZ
+
+                    if (diffX == 0.0 || diffZ == 0.0) {
+                        player.isGliding = false
+                        glideMap -= player
+                    }
+
+                    newX to newZ
+                }
+            }
         }
     }
 
     override suspend fun onActivate(player: Player) {
-        startGliding(player)
-        player.velocity.add(player.location.direction.multiply(3))
         player.playSound(SOUND.first, SOUND.second, SOUND.third)
 
         sync {
             player.addPotionEffect(
-                PotionEffectBuilder.build {
+                dslMutator<PotionEffectBuilder> {
                     type = PotionEffectType.LEVITATION
-                    durationInt = Integer.MAX_VALUE
+                    duration = Duration.INFINITE
                     ambient = true
                     key = NamespacedKey("terix", KEY)
-                }
+                }.asNew().get()
             )
         }
     }
 
     override suspend fun onDeactivate(player: Player) {
-        glideMap -= player.uniqueId
+        glideMap -= player
         val types = mutableListOf<PotionEffectType>()
         for (potion in player.activePotionEffects) {
             if (potion.type != PotionEffectType.LEVITATION || potion.key?.key != KEY) continue
@@ -59,11 +94,11 @@ class Levitate : Ability(AbilityType.TOGGLE) {
     }
 
     private fun startGliding(player: Player) {
-        if (glideMap.contains(player.uniqueId)) return
+        if (glideMap.contains(player)) return
 
         sentryBreadcrumb(SCOPE, "levitate.gliding.start")
 
-        glideMap += player.uniqueId
+        glideMap += player
         player.isGliding = true
     }
 
