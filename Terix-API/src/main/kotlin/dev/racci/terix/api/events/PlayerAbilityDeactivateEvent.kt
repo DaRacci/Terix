0b2e4c7ae1abbe9aaf3e85fef62ccac5f41191cd
DaRacci <racci@racci.dev>
2022-10-08 17:33:12 +1100

    refactor(Events): Use CompanionEventHandler

--- Terix-API/src/main/kotlin/dev/racci/terix/api/events/PlayerAbilityDeactivateEvent.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/events/PlayerAbilityDeactivateEvent.kt
@@ -1,16 +1,14 @@
 package dev.racci.terix.api.events
 
-import dev.racci.minix.api.events.KEvent
+import dev.racci.minix.api.events.CompanionEventHandler
 import dev.racci.terix.api.origins.abilities.Ability
 import org.bukkit.entity.Player
-import org.bukkit.event.HandlerList
 
 class PlayerAbilityDeactivateEvent(
     player: Player,
     ability: Ability
 ) : PlayerAbilityEvent(player, ability) {
-    companion object {
-        @JvmStatic
-        fun getHandlerList(): HandlerList = KEvent.handlerMap[PlayerOriginChangeEvent::class]
+    companion object : CompanionEventHandler() {
+        @JvmStatic override fun getHandlerList() = super.getHandlerList()
     }
 }
