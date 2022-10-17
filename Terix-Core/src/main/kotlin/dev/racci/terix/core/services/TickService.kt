36d954738379e527cb04e0a6b333a41b5f4028e6
DaRacci <racci@racci.dev>
2022-10-17 15:37:09 +1100

    refactor(RunnableService): Merged into tick service + other cleanup

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/services/TickService.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/services/TickService.kt
@@ -1,13 +1,18 @@
 package dev.racci.terix.core.services
 
+import arrow.core.Either
+import arrow.core.Predicate
+import arrow.core.left
+import arrow.core.right
 import dev.racci.minix.api.annotations.MappedExtension
 import dev.racci.minix.api.data.enums.LiquidType
 import dev.racci.minix.api.data.enums.LiquidType.Companion.liquidType
 import dev.racci.minix.api.extension.Extension
+import dev.racci.minix.api.extension.ExtensionState
+import dev.racci.minix.api.extensions.collections.clear
+import dev.racci.minix.api.extensions.collections.computeAndRemove
 import dev.racci.minix.api.extensions.event
 import dev.racci.minix.api.extensions.onlinePlayers
-import dev.racci.minix.api.utils.Closeable
-import dev.racci.minix.api.utils.kotlin.doesOverride
 import dev.racci.minix.api.utils.ticks
 import dev.racci.minix.nms.aliases.NMSWorld
 import dev.racci.minix.nms.aliases.toNMS
@@ -16,6 +21,7 @@ import dev.racci.terix.api.Terix
 import dev.racci.terix.api.TerixPlayer
 import dev.racci.terix.api.events.PlayerOriginChangeEvent
 import dev.racci.terix.api.origins.origin.Origin
+import dev.racci.terix.api.origins.states.State
 import dev.racci.terix.core.extensions.inDarkness
 import dev.racci.terix.core.extensions.inRain
 import dev.racci.terix.core.extensions.inSunlight
@@ -24,16 +30,24 @@ import dev.racci.terix.core.extensions.wasInDarkness
 import dev.racci.terix.core.extensions.wasInRain
 import dev.racci.terix.core.extensions.wasInSunlight
 import dev.racci.terix.core.extensions.wasInWater
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import dev.racci.terix.core.services.runnables.AmbientTick
+import dev.racci.terix.core.services.runnables.ChildTicker
+import dev.racci.terix.core.services.runnables.DarknessTick
+import dev.racci.terix.core.services.runnables.MotherTicker
+import dev.racci.terix.core.services.runnables.RainTick
+import dev.racci.terix.core.services.runnables.SunlightTick
+import dev.racci.terix.core.services.runnables.WaterTick
+import kotlinx.collections.immutable.persistentHashMapOf
+import kotlinx.collections.immutable.toPersistentHashSet
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.yield
@@ -42,65 +56,73 @@ import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.levelgen.Heightmap
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.inventory.PlayerInventory
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.roundToInt
 
-@MappedExtension(Terix::class, "Tick Service", [OriginService::class])
+@MappedExtension(Terix::class, "Tick Service", [OriginService::class], threadCount = 4)
 class TickService(override val plugin: Terix) : Extension<Terix>() {
     private val delayChannel = Channel<Unit>(0)
-    private val originReference = HashMap<Player, Origin>()
     private val mutex = Mutex()
     private val playerQueue = ArrayDeque<Player>()
     private val internalFlow = MutableSharedFlow<Player>()
-    private val tickerThreads = object : Closeable<ExecutorCoroutineDispatcher>() {
-        @OptIn(DelicateCoroutinesApi::class)
-        override fun create(): ExecutorCoroutineDispatcher {
-            return newFixedThreadPoolContext(4, "Origin Ticker")
-        }
-    }
+    private val motherTickers = mutableMapOf<Player, MotherTicker>()
+    private var tickables = persistentHashMapOf<Either<State, Predicate<Origin>>, (Player, Origin) -> ChildTicker>()
 
     /** The queue of players which have disconnected. (Slightly faster performance than checking [Player.isOnline]) */
-    private val removeQueue = HashSet<UUID>()
+    private val removeQueue = ConcurrentHashMap.newKeySet<UUID>()
 
     val playerFlow = internalFlow.asSharedFlow()
 
     override suspend fun handleEnable() {
-        event<PlayerJoinEvent> {
-            delayChannel.trySend(Unit)
-            removeQueue.remove(player.uniqueId) // Maybe they reconnected?
-            mutex.withLock { playerQueue.addLast(player) }
+        // TODO -> Only load these once
+        startTicker()
+        startInternalTickListeners()
 
+        tickables = persistentHashMapOf(
+            State.LightState.SUNLIGHT.left() to ::SunlightTick,
+            State.LightState.DARKNESS.left() to ::DarknessTick,
+            State.WeatherState.RAIN.left() to ::RainTick,
+            State.LiquidState.WATER.left() to ::WaterTick,
+            { origin: Origin -> origin.sounds.ambientSound != null }.right() to ::AmbientTick
+        )
+
+        onlinePlayers.forEach { player -> rehabMother(player, TerixPlayer.cachedOrigin(player)) }
+
+        event<PlayerJoinEvent>(EventPriority.MONITOR, true) {
             val origin = TerixPlayer.cachedOrigin(player)
-            if (hasTick(origin)) { originReference[player] = origin }
+
+            rehabMother(player, origin)
+
+            if (!removeQueue.remove(player.uniqueId)) {
+                mutex.withLock { playerQueue.addLast(player) }
+            }
+
+            delayChannel.trySend(Unit)
         }
+
         event<PlayerQuitEvent> {
-            originReference.remove(player)
             removeQueue.add(player.uniqueId)
         }
+
         event<PlayerOriginChangeEvent> {
             val origin = TerixPlayer.cachedOrigin(player)
-            if (hasTick(origin)) { originReference[player] = origin } else { originReference.remove(player) }
+            rehabMother(player, origin)
         }
-
-        startTicker()
-        startTickListener()
     }
 
-    override suspend fun handleUnload() {
-        tickerThreads.close()
+    override suspend fun handleDisable() {
+        motherTickers.clear { _, mother -> mother.endSuffering() }
     }
 
-    private fun hasTick(origin: Origin): Boolean {
-        return origin::class.doesOverride(Origin::onTick.name)
-    }
-
-    private suspend fun startTicker() = launch(tickerThreads.get()) {
-        val flow = flow {
-            while (loaded) {
-                if (onlinePlayers.isEmpty()) delayChannel.receive() // Wait for players to join.
+    private suspend fun startTicker() = launch(dispatcher.get()) {
+        flow {
+            while (state != ExtensionState.DISABLED) {
+                if (!loaded || onlinePlayers.isEmpty()) delayChannel.receive() // Wait for players to join or for this to load.
 
                 if (playerQueue.isEmpty()) {
                     delay(10); continue
@@ -109,24 +131,24 @@ class TickService(override val plugin: Terix) : Extension<Terix>() {
                 val player = mutex.withLock { playerQueue.removeFirst() }
                 emit(player)
             }
-        }
-
-        flow.onEach(::runTicker)
+        }.onEach(::runTicker)
             .buffer()
-            .collect(internalFlow::emit)
-    }
-
-    private fun startTickListener() = launch(tickerThreads.get()) {
-        playerFlow
-            .onEach { player -> originReference[player]?.onTick(player) }
+            .onEach(internalFlow::emit)
             .collect { player ->
                 async {
                     yield()
-                    delay(1.ticks)
-                    if (player.uniqueId in removeQueue) return@async
+                    delay(TICK_RATE.ticks)
+                    if (removeQueue.remove(player.uniqueId)) return@async
                     mutex.withLock { playerQueue.addLast(player) }
                 }
             }
+    }
+
+    private fun startInternalTickListeners() = launch(dispatcher.get()) {
+        playerFlow
+            .conflate()
+            .onEach { player -> TerixPlayer.cachedOrigin(player).onTick(player) }
+            .mapNotNull(motherTickers::get).collect(MotherTicker::run)
     }
 
     private fun runTicker(
@@ -183,5 +205,41 @@ class TickService(override val plugin: Terix) : Extension<Terix>() {
         brightness > 0.5f &&
         level.isDay
 
-    companion object : ExtensionCompanion<TickService>()
+    private fun rehabMother(
+        player: Player,
+        origin: Origin
+    ) {
+        motherTickers.computeAndRemove(player, MotherTicker::endSuffering)
+
+        val states = presentStates(origin)
+        val children = mutableSetOf<ChildTicker>()
+        for ((key, constructor) in tickables) {
+            if (!key.fold(
+                    { state -> states.contains(state) },
+                    { predicate -> predicate(origin) }
+                )
+            ) continue
+
+            children += constructor(player, origin)
+        }
+
+        if (children.isEmpty()) {
+            logger.debug { "No tickables for ${player.name}." }
+            return
+        }
+
+        motherTickers[player] = MotherTicker(this, children.toPersistentHashSet())
+    }
+
+    private fun presentStates(origin: Origin): Sequence<State> = sequence {
+        yieldAll(origin.statePotions.keys)
+        yieldAll(origin.stateDamageTicks.keys)
+        yieldAll(origin.stateTitles.keys)
+        yieldAll(origin.stateBlocks.keys)
+        yieldAll(origin.attributeModifiers.keys)
+    }.distinct()
+
+    companion object : ExtensionCompanion<TickService>() {
+        const val TICK_RATE = 2
+    }
 }
