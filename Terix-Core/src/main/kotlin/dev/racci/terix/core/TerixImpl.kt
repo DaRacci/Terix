22369b93b9327a78f1b7f37db4dbf85104c1eb85
DaRacci <racci@racci.dev>
2022-11-01 22:44:32 +1100

    fix: Activate listeners

--- Terix-Core/src/main/kotlin/dev/racci/terix/core/TerixImpl.kt
+++ Terix-Core/src/main/kotlin/dev/racci/terix/core/TerixImpl.kt
@@ -3,10 +3,12 @@ package dev.racci.terix.core
 import dev.racci.minix.api.annotations.MappedPlugin
 import dev.racci.terix.api.Terix
 import dev.racci.terix.core.integrations.TempPlaceholderExpansion
+import org.incendo.interfaces.paper.PaperInterfaceListeners
 
 @MappedPlugin(14443, Terix::class)
-class TerixImpl : Terix() {
+public class TerixImpl : Terix() {
     override suspend fun handleLoad() {
+        PaperInterfaceListeners.install(this)
         TempPlaceholderExpansion(this).register()
     }
 }
