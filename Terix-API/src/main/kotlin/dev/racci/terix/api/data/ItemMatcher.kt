05d6adbe117c47f67952c1c0f60e0af3c852aab9
DaRacci <racci@racci.dev>
2022-11-03 13:36:33 +1100

    fix: Stupid JVM signatures

--- Terix-API/src/main/kotlin/dev/racci/terix/api/data/ItemMatcher.kt
+++ Terix-API/src/main/kotlin/dev/racci/terix/api/data/ItemMatcher.kt
@@ -4,11 +4,9 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 public fun interface ItemMatcher {
-    @Suppress("INAPPLICABLE_JVM_NAME")
-    @JvmName("matchesReceiver")
-    public fun ItemStack.matches(): Boolean
+    public fun ItemStack.actualMatcher(): Boolean
 
-    public fun matches(stack: ItemStack): Boolean = stack.matches()
+    public fun matches(stack: ItemStack): Boolean = stack.actualMatcher()
 
     public companion object {
         public fun of(material: Material): ItemMatcher = ItemMatcher { this.type === material }
